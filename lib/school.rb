93b5b9fbe6b2366cab907e20e5a2ff798a2ed86e
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-13 15:27:45 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+            # binding.pry
+        end
+        @roster[grade].push(name)
+        # binding.pry
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each{|key,value|
+            value.sort!
+        }
+    end
+
+end
